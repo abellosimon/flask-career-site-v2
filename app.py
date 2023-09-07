fd6731afa1a2621608e345329192b4ee694b0441
abellosimon <143218153+abellosimon@users.noreply.github.com>
2023-09-07 10:59:30 +0000

    Added dynamic data and api route

--- app.py
+++ app.py
@@ -1,13 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [{
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Rotterdam, Netherlands',
+    'salary': 'EUR 80,000'
+}, {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Amsterdam, Netherlands',
+}, {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote',
+    'salary': 'EUR 100,000'
+}, {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'San Francisco, USA',
+    'salary': 'USD 150,000'
+}]
 
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, company_name='Simon')
 
-#TODO: Learn to change favicon icon
+
+@app.route("/api//jobs")
+def list_jobs():
+  return jsonify(JOBS)
+
+
+#DONE: Learn to change favicon icon
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
