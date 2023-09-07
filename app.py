1baa8526fe6b0ac4a82db10a42b28c46cbaeeacf
abellosimon <143218153+abellosimon@users.noreply.github.com>
2023-09-07 08:05:43 +0000

    added html and css
    added html and css

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-  return "Hello World I am cool"
+  return render_template('home.html')
+
+#TODO: Learn to change favicon icon
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
