80e7b0eaa05ee3f8e20407bc73f319e77b154d46
abellosimon <143218153+abellosimon@users.noreply.github.com>
2023-09-18 10:19:05 +0000

    added dynamic pages for each job

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, render_template
 
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
   jobs_db = load_jobs_from_db()
-  return render_template('home.html', jobs=jobs_db, company_name='Simon')
+  return render_template('home.html', jobs=jobs_db)
 
 
 @app.route("/api/jobs")
@@ -17,7 +17,19 @@ def list_jobs():
   return jsonify(jobs_db)
 
 
-#DONE: Learn to change favicon icon
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  if not job:
+    return "Not Found", 404
+  return render_template("jobpage.html", job=job)
+
+
+@app.route("/api/job/<id>")
+def show_job_api(id):
+  job = load_job_from_db(id)
+  return jsonify(job)
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,5 +1,6 @@
-from sqlalchemy import create_engine, text
 import os
+
+from sqlalchemy import create_engine, text
 
 engine = create_engine(
     os.environ['DB_CONNECTION_STRING'],
@@ -15,3 +16,18 @@ def load_jobs_from_db():
     for row in result.all():
       jobs.append(dict(row._mapping))
     return jobs
+
+
+def load_job_from_db(id):
+  try:
+    with engine.connect() as conn:
+      result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"),
+                            {'val': id})
+      row = result.fetchone()
+      if row is None:
+        return None
+      else:
+        return row._asdict()
+  except Exception as e:
+    print(f"Error fetching job with ID {id}: {e}")
+    return None
